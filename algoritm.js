939f96972f214e67f23b69d726aaa4891c965ef6
Прушак Евгений Владимирович <D:\poshta>
2019-04-02 17:22:07 +0300

    fit(add sort): added merge and bubble sort

--- algoritm.js
+++ algoritm.js
@@ -22,23 +22,111 @@ function  sum( a, b) {
 sum(3, 1); // 4
 // ---------------------------
 
-// Big o natation
+// Big O natation 
+// Основная база алгоритмов https://github.com/trekhleb/javascript-algorithms
+// O(n!) -  very bad algoritm
+// O(n*n) - bad algoritm
+//O(n*logn) - not good algoritm
+//O(n) - not good algoritm
+//O(logn) - good algoritm
+// O(1) - good algoritm
 
-// числа Фибоначчи
+//------------------------------------
 
-//---Bogosort
-function Bogosort(arr) {
-    var isSorted = function (arr) {
-        for (let i = 1; i < arr.length; i++) {
-           if (arr[i - 1] > arr[i]) {
-               return false;
-           }
-            
-        }
-        return true;
-    };
+// number fibonnacci
 
-    function shuffler(arr) {
-        
+export default function fibonacci(n) {
+    if (n <= 2) {
+        return 1;
+    } else {
+        return fibonacci (n - 1) + fibonacci ( n -2);
     }
 }
+
+let invoke = 0;
+// расход memory 109 O(n*n)
+function fibonacciSumm(n) {
+    invoke++;
+    if (n <= 2) {
+        return 1;
+    } else {
+        return fibonacciSumm( n - 1) + fibonacciSumm( n - 2);
+    }
+}
+// расход memory 9, O(n)
+function fibonacci(n) {
+    const fibSequence = [1];
+    let currentValue = 1;
+    let previousValue = 0;
+    if (n === 1) {
+        return fibSequence;
+    }
+    let iterationsCounter = n - 1;
+    while (iterationsCounter) {
+        invoke++;
+        currentValue += previousValue;
+        previousValue = currentValue - previousValue;
+        fibSequence.push(currentValue);
+        iterationsCounter -= 1;
+    }
+    return fibSequence;
+}
+
+//-----------------------------------
+
+
+//-----Bubble Sort-----------------------
+// memory not match, O(n) - best, O(n*n) - normal version
+
+var a =[33, 103, 3, 726, 200, 984, 198, 764, 9];
+function bubbleSort(a) {
+    var swapped;
+
+    do{ 
+        swapped = false;
+
+        for (let i = 0; i < a.length; i++) 
+        {   if (a[i] > a[i + 1]) {
+            var temp = a[i];
+            a[i] = a[i + 1];
+            a[i + 1] = temp;
+            swapped = true;
+        }
+        }
+    } 
+    while (swapped);
+}
+
+//----Merge Sort------- O(n*logn)
+function mergeSort(arr) {
+    arr = [1, 57, 88, 404, 505, 2, 7, 200, 3, 1220, 300];
+    if (arr.length === 1) {
+        return arr;
+    } 
+const middle = Math.floor(arr.length / 2);
+const left = arr.slice(0, middle);
+const rigth = arr.slice(middle);
+return merge(    mergeSort(left),        mergeSort(rigth) );
+};
+
+function merge(left, right) {
+    let result = [];
+    let indexLeft = 0;
+    let indexRight = 0;
+    while ( indexLeft < left.length && 
+        indexRight < right.length) {
+        if (left[indexLeft] < right[indexRight]) {
+            result.push(left[indexLeft]);
+            indexLeft++;
+        } else {
+            result.push(right[indexRight]);
+            indexRight++;
+        }
+    }
+    return result.concat(left.slice(indexLeft)).concat(right.slice(indexRight));
+}
+
+
+
+
+//------------------------------
