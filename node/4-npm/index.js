e714bcd7bfb28911240cfb48cee51ba49717fe3b
Прушак Евгений Владимирович <D:\poshta>
2019-04-05 17:16:09 +0300

    fit: add 5-app (scraping )

---
+++ node/4-npm/index.js
@@ -0,0 +1,11 @@
+const _ = require('lodash');
+
+// const user = {
+//     name: 'Evgen',
+//     age: 20,
+//     language: 'english'
+// }
+
+// const userWithoutAge = _.unset(user, 'age');
+
+// console.log(user);
