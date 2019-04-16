d74aa37504bec0638d7c96c2bf3f1cf210f6e5e8
Прушак Евгений Владимирович <D:\poshta>
2019-04-16 12:23:10 +0300

    fit: node: 4-npm and 5-npm

--- JsFunction/index.js
+++ JsFunction/index.js
@@ -47,38 +47,76 @@
 
 
 //------es2015---------
-const btn = document.querySelector('button');
+// const btn = document.querySelector('button');
 
 
-class Army  {
-    constructor(startAmount){
-        this.stormtroopers =  startAmount || 0;
-        this.replenish = () => {
-            this.stormtroopers += 1000;
-            this.replenish += addRandomClones(100, 200);
-        };
+// class Army  {
+//     constructor(startAmount){
+//         this.stormtroopers =  startAmount || 0;
+//         this.replenish = () => {
+//             this.stormtroopers += 1000;
+//             this.replenish += addRandomClones(100, 200);
+//         };
     
-    }
+//     }
+// };
+// const  army = new Army(0);
+
+// const addRandomClones =  (...args) => {
+   
+//     return Math.floor(Math.random() * (args[1] - args[0]) + args[0]);
+// }
+
+
+// army.replenish();
+// //const repl = army.replenish.bind(army);
+
+
+
+
+// btn.addEventListener('click', () => {
+//     army.replenish();
+//     if (army.stormtroopers > 10000) {
+//        alert('галактика у Ваших ног');
+//        army.stormtroopers = 0;
+//     }
+//     document.getElementById('stormtroopers').innerText = army.stormtroopers;
+// });
+
+//------scope---------
+const smallArmy = {
+    fighters: 400,
+}
+function showArmy() {
+    const smallArmy = {
+    fighters: 200,
 };
-const  army = new Army(0);
+
+console.log('army', this);
+}
+
+
+
+const Army = function (name) {
+    this.name = name;
+    this.stormtroopers = 1000;
+    this.replenish = function () {
+        //console.log(this);
+        this.stormtroopers += addRandomClones(100, 200);
+    };
+    this.showArmy = showArmy;
+};
+
+const  army1 = new Army("Imperial");
+const  army2 = new Army("Imperial");
 
 const addRandomClones =  (...args) => {
    
-    return Math.floor(Math.random() * (arguments[1] - arguments[0]) + arguments[0]);
+    return Math.floor(Math.random() * (args[1] - args[0]) + args[0]);
 }
 
 
 army.replenish();
-//const repl = army.replenish.bind(army);
 
-
-
-
-btn.addEventListener('click', () => {
-    army.replenish();
-    if (army.stormtroopers > 10000) {
-       alert('галактика у Ваших ног');
-       army.stormtroopers = 0;
-    }
-    document.getElementById('stormtroopers').innerText = army.stormtroopers;
-});
+army1.showArmy();
+army2.showArmy();
