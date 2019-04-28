dd166273b7c22cf2666f79e55792ff82c5a2eea4
EvgenPrushk <ogmprushak@mail.ru>
2019-04-28 16:07:12 +0300

    feat: dom

---
+++ DOM/data.js
@@ -0,0 +1,272 @@
+window.data = [
+    {
+      "_id": "5cc58538534b838f4306d6a3",
+      "index": 0,
+      "guid": "c172846c-f02f-4f82-b19d-55e61317435d",
+      "isActive": true,
+      "balance": "$1,536.93",
+      "picture": "http://placehold.it/32x32",
+      "age": 37,
+      "eyeColor": "blue",
+      "name": "England Osborne",
+      "gender": "male",
+      "company": "BRISTO",
+      "email": "englandosborne@bristo.com",
+      "phone": "+1 (897) 591-3057",
+      "address": "725 Eastern Parkway, Southview, Tennessee, 2184",
+      "about": "Laboris eiusmod adipisicing enim consequat ut quis cillum. Id ex dolor ex est irure enim irure magna eu labore. Laboris nulla officia enim do minim nulla elit sit officia dolore quis adipisicing.\r\n",
+      "registered": "2018-10-07T08:39:09 -03:00",
+      "latitude": -72.817964,
+      "longitude": 141.111965,
+      "tags": [
+        "magna",
+        "tempor",
+        "proident",
+        "velit",
+        "ullamco",
+        "cillum",
+        "dolor"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Burks Hurley"
+        },
+        {
+          "id": 1,
+          "name": "Josefa Miranda"
+        },
+        {
+          "id": 2,
+          "name": "Delgado Cohen"
+        }
+      ],
+      "greeting": "Hello, England Osborne! You have 2 unread messages.",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "_id": "5cc585384fb2fb35ebce1d16",
+      "index": 1,
+      "guid": "4e71efad-475c-4593-8dbb-9e320cbe9f92",
+      "isActive": false,
+      "balance": "$3,900.60",
+      "picture": "http://placehold.it/32x32",
+      "age": 40,
+      "eyeColor": "green",
+      "name": "Leonor Monroe",
+      "gender": "female",
+      "company": "TYPHONICA",
+      "email": "leonormonroe@typhonica.com",
+      "phone": "+1 (971) 566-3250",
+      "address": "541 High Street, Allentown, Palau, 8628",
+      "about": "Laborum occaecat enim qui culpa anim ad excepteur ut mollit occaecat reprehenderit. Commodo irure nisi commodo enim adipisicing enim culpa. Elit non et sit duis enim duis culpa et. Nisi pariatur est occaecat nostrud cupidatat esse sunt. Aliquip dolore irure cillum reprehenderit aute ullamco sint. Sit excepteur ullamco aliquip laboris proident sit consequat nulla minim veniam culpa velit et aute. Ea nisi commodo reprehenderit aute exercitation reprehenderit qui tempor.\r\n",
+      "registered": "2019-03-06T06:38:13 -02:00",
+      "latitude": -15.210497,
+      "longitude": -129.24322,
+      "tags": [
+        "minim",
+        "nulla",
+        "deserunt",
+        "laboris",
+        "enim",
+        "velit",
+        "fugiat"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Debra Farrell"
+        },
+        {
+          "id": 1,
+          "name": "Hess Wynn"
+        },
+        {
+          "id": 2,
+          "name": "Silvia Parrish"
+        }
+      ],
+      "greeting": "Hello, Leonor Monroe! You have 5 unread messages.",
+      "favoriteFruit": "apple"
+    },
+    {
+      "_id": "5cc58538fa7eef3fa9041a5e",
+      "index": 2,
+      "guid": "0ba9d36d-0a9f-410e-bf3b-1934dfda04ce",
+      "isActive": false,
+      "balance": "$3,460.08",
+      "picture": "http://placehold.it/32x32",
+      "age": 40,
+      "eyeColor": "blue",
+      "name": "Rosalind Mccray",
+      "gender": "female",
+      "company": "DADABASE",
+      "email": "rosalindmccray@dadabase.com",
+      "phone": "+1 (990) 440-3357",
+      "address": "693 Wilson Avenue, Alden, Indiana, 1271",
+      "about": "Incididunt magna aliqua labore nisi dolor velit do ex dolor. Commodo voluptate mollit ad exercitation qui qui velit pariatur nisi duis. Ea eiusmod pariatur laborum in mollit et nostrud nulla nisi. Voluptate ipsum aliqua dolore Lorem reprehenderit labore dolor minim do laboris. Velit commodo Lorem Lorem voluptate cillum proident mollit id sint aliqua est. Exercitation sint nostrud sit eiusmod culpa ut enim id esse consequat dolor.\r\n",
+      "registered": "2016-04-15T09:30:37 -03:00",
+      "latitude": -63.546117,
+      "longitude": -54.377687,
+      "tags": [
+        "mollit",
+        "qui",
+        "laboris",
+        "eiusmod",
+        "labore",
+        "ullamco",
+        "voluptate"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Kirkland Paul"
+        },
+        {
+          "id": 1,
+          "name": "Janis Levy"
+        },
+        {
+          "id": 2,
+          "name": "Santos Brady"
+        }
+      ],
+      "greeting": "Hello, Rosalind Mccray! You have 5 unread messages.",
+      "favoriteFruit": "banana"
+    },
+    {
+      "_id": "5cc58538e903ce82fd67f6f4",
+      "index": 3,
+      "guid": "b57103dd-fd64-46d4-aa56-73b229da3a2b",
+      "isActive": true,
+      "balance": "$3,713.09",
+      "picture": "http://placehold.it/32x32",
+      "age": 20,
+      "eyeColor": "green",
+      "name": "Sherry Gilliam",
+      "gender": "female",
+      "company": "OPTIQUE",
+      "email": "sherrygilliam@optique.com",
+      "phone": "+1 (859) 416-2371",
+      "address": "841 Beverly Road, Edgewater, Puerto Rico, 5204",
+      "about": "Esse aute est eiusmod voluptate in consequat do et laborum ipsum aliquip quis ut. Mollit reprehenderit ad ea aliquip cupidatat sunt minim eiusmod laborum consectetur ex ex laboris. Aute dolor fugiat excepteur veniam eu. In ex enim proident adipisicing est tempor nostrud do eu tempor.\r\n",
+      "registered": "2015-06-10T05:29:35 -03:00",
+      "latitude": -3.038968,
+      "longitude": -32.940603,
+      "tags": [
+        "eiusmod",
+        "culpa",
+        "amet",
+        "labore",
+        "aute",
+        "fugiat",
+        "dolor"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Marci Mccarty"
+        },
+        {
+          "id": 1,
+          "name": "Margarita Fry"
+        },
+        {
+          "id": 2,
+          "name": "Jolene Kaufman"
+        }
+      ],
+      "greeting": "Hello, Sherry Gilliam! You have 10 unread messages.",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "_id": "5cc5853862f04b0921738161",
+      "index": 4,
+      "guid": "9d112cef-fe0a-4ab4-8bfc-74414ed9f943",
+      "isActive": true,
+      "balance": "$2,110.96",
+      "picture": "http://placehold.it/32x32",
+      "age": 23,
+      "eyeColor": "blue",
+      "name": "Beatrice Terrell",
+      "gender": "female",
+      "company": "WRAPTURE",
+      "email": "beatriceterrell@wrapture.com",
+      "phone": "+1 (941) 527-2039",
+      "address": "382 Emerald Street, Shelby, Illinois, 6240",
+      "about": "Qui reprehenderit non ipsum sit esse dolore laboris. Reprehenderit nulla tempor eiusmod et eiusmod laborum voluptate amet. Ex nulla labore irure ipsum et esse cupidatat sint quis aliquip tempor tempor. Id qui magna qui dolor occaecat qui ut. Qui do velit ipsum dolor incididunt ipsum eu nulla officia esse sunt nisi enim sint.\r\n",
+      "registered": "2014-05-27T10:28:48 -03:00",
+      "latitude": -77.102849,
+      "longitude": 80.450895,
+      "tags": [
+        "aliquip",
+        "est",
+        "sunt",
+        "cillum",
+        "ad",
+        "ea",
+        "eu"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Bettie Woods"
+        },
+        {
+          "id": 1,
+          "name": "Fern Cross"
+        },
+        {
+          "id": 2,
+          "name": "Summers Ratliff"
+        }
+      ],
+      "greeting": "Hello, Beatrice Terrell! You have 8 unread messages.",
+      "favoriteFruit": "apple"
+    },
+    {
+      "_id": "5cc585383e54eac2ecff7d2d",
+      "index": 5,
+      "guid": "24368749-665e-447c-bd8a-f6e9b6f67a41",
+      "isActive": true,
+      "balance": "$2,335.60",
+      "picture": "http://placehold.it/32x32",
+      "age": 32,
+      "eyeColor": "green",
+      "name": "Short Cooley",
+      "gender": "male",
+      "company": "PAWNAGRA",
+      "email": "shortcooley@pawnagra.com",
+      "phone": "+1 (828) 522-3089",
+      "address": "871 Hamilton Avenue, Chesapeake, Missouri, 2750",
+      "about": "Mollit consequat quis fugiat fugiat. Minim proident labore sit occaecat quis adipisicing id velit. Mollit enim est Lorem amet occaecat commodo labore. Aute Lorem ea enim irure irure.\r\n",
+      "registered": "2014-11-25T12:26:24 -02:00",
+      "latitude": 65.445739,
+      "longitude": 76.792342,
+      "tags": [
+        "tempor",
+        "laboris",
+        "est",
+        "anim",
+        "labore",
+        "velit",
+        "culpa"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Oconnor Padilla"
+        },
+        {
+          "id": 1,
+          "name": "Elva Huffman"
+        },
+        {
+          "id": 2,
+          "name": "Shelley Phillips"
+        }
+      ],
+      "greeting": "Hello, Short Cooley! You have 1 unread messages.",
+      "favoriteFruit": "strawberry"
+    }
+  ]
--- DOM/dom.js
+++ DOM/dom.js
@@ -54,10 +54,64 @@
 // // EVENT stopPropagation - прерывается всплытие или погружение эвента
 
 // // обработчик на li 
-let counter = 0;
-const button = document.querySelector('button');
-const span = document.querySelector('span');
-button.addEventListener('click', e => {
-    counter++;
-    span.innerHTML = counter;
+// let counter = 0;
+// const button = document.querySelector('button');
+// const span = document.querySelector('span');
+// button.addEventListener('click', e => {
+//    if (e.altKey) {
+//     counter +=10;
+//    } else {
+//     counter++;
+//    }
+   
+//     span.innerHTML = counter;
+// });
+// span.innerHTML = counter;
+
+const ul = document.createElement('ul');
+const input = document.createElement('input');
+input.type = 'text';
+const button = document.createElement('button');
+button.innerHTML = 'Add user';
+
+function renderItems(container, items) {
+    container.innerHTML =  items
+.map(item => {
+   return `
+   <li class="${item.removed ? '-removed' : 
+   ''}">${item.name}</li>`})
+.join('\n');
+}
+
+renderItems(ul, window.data);
+
+document.body.appendChild(input);
+document.body.appendChild(button);
+document.body.appendChild(ul);
+
+ul.addEventListener('click', e => {
+    e.target.classList.toggle('-removed');
+    const index = Array.from(ul.children).indexOf(e.target);
+    
+    window.data [index].removed = !window.data[index].removed;
+});
+
+// function renderItems2(counter, items) {
+//     items.forEach(item => {
+//         const li = document.createElement('li');
+//         if (item.removed) {
+//             li.classList.add('-removed');
+//         }
+//         li.textContent = item.name;
+//         container.appendChild(li);
+//     })
+// }
+
+button.addEventListener('click', () =>{
+    window.data.push({
+        name: input.value
+});
+
+renderItems(ul, window.data);
+bindListeners();
 });
