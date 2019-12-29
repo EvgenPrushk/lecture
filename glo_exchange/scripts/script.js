445781acf8e9943f9326e402da47951a201b8c32
Evgen Prushak <ogmprushak@mail.ru>
2019-12-30 01:57:22 +0300

    fit: add  index.html +script

---
+++ glo_exchange/scripts/script.js
@@ -0,0 +1,115 @@
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+
+    const     customer = document.getElementById('customer'),
+    freelancer = document.getElementById('freelancer'),
+    blockCustomer = document.getElementById('blockCustomer'),
+    blockFreelancer = document.getElementById('block-customer'),
+    blockChoice = document.getElementById('block-choice'),
+    btnExit = document.getElementById('btn-exit'),
+    ordersTable = document.getElementById('orders'),
+    modalOrder = document.getElementById('order_read'),
+    modalOrderActive = document.getElementById('order_active');
+
+    const orders =JSON.parse(localStorage.getItem('freeOrders')) || [];
+
+    const toStorage = () => {
+        localStorage.setItem('freeOrders', JSON.stringify(orders));
+    };
+
+    const calcDeadline = (deadline) => {
+        const day = '10 дней';
+        return day;
+    }
+
+
+    const renderOrders = () => {
+        ordersTable.textContent = '';
+        orders.forEach((order, i) => {
+            ordersTable.innerHTML += `
+            <tr class="order ${order.active ? 'taken' : '' }"
+                data-number-order = "${i}">
+                <td>${i+1}</td>
+                <td>${order.title}</td>
+                <td class="${order.currency}"></td>
+                <td>${calcDeadline(order.deadline)}</td>
+            </tr>`;
+        });
+    };
+
+    const handlerModal = (event) => {
+        const target = event.target;
+        const modal = target.closest('.order-modal');
+        const order = orders[modal.id];
+
+        const baseAction = () => {
+            modal.style.display = 'none';
+            toStorage();
+            renderOrders();
+        }
+
+        if (target.closest('.close') || target === modal) {
+            modal.style.display = 'none';
+        }
+
+        if (target.classList.contains('get-order')) {
+            order.active = true;
+            baseAction();
+        }
+
+        if (target.id === 'capitulation') {
+            order.active = false;
+            baseAction();
+        }
+
+        if (target.id === 'ready') {
+            ordar.splice(orders.indexOf(order), 1);
+            baseAction();
+        }
+    }
+
+    const openModal = (numberOrder) => {
+        const order = orders[numberOrder];
+        const {title, firstName, email, phone, description, amount,
+        currency, deadline, active = false } = order;
+
+        const modal = active ? modalOrderActive : modalOrder;
+
+        const firstNameBlock = modal.querySelector('.firstName'),
+        titleBlock = modal.querySelector('.modal-title'),
+        emailBlock = modal.querySelector('.email'),
+        descriptionBlock = modal.querySelector('.description'),
+        deadlineBlock = modal.querySelector('.deadline'),
+        currencyBlock = modal.querySelector('.currency_img'),
+        countBlock = modal.querySelector('.count'),
+        phoneBlock = modal.querySelector('.phone');
+    
+        modal.id = numberOrder;
+        titleBlock.textContent = title;
+        firstNameBlock.textContent = firstName;
+        emailBlock.textContent = email;
+        emailBlock.href = 'mailto:' + email;
+        descriptionBlock.textContent = description;
+        deadlineBlock.textContent = calcDeadline(deadline);
+        currencyBlock.className = 'currency_img';
+        currencyBlock.classList.add(currency);
+        countBlock.textContent = amount;
+    
+    
+        phoneBlock && (phoneBlock.href = 'tel:' + phone);
+    
+        modal.style.display = 'flex';
+
+        modal.addEventListener('click', handlerModal);
+         
+    
+    };
+
+    
+
+
+    
+
+
+
+});
